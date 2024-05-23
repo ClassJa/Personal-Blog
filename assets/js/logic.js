8279f320e5973f15fc94dd8d378652c1454cb353
JA <69608172+ClassJay@users.noreply.github.com>
2024-05-23 17:01:26 -0500

    working through adding dark mode toggle so it is functional

---
+++ assets/js/logic.js
@@ -0,0 +1,26 @@
+const themeSwitcher = document.querySelector('#theme-switcher');
+const container = document.querySelector('.container');
+
+let mode = 'dark';
+
+
+themeSwitcher.addEventListener('click', function(){
+    if (mode === 'dark') {
+        mode = 'light';
+        container.setAttribute('class', 'light');
+    } else {
+        mode = 'dark'
+        container.setAttribute('class', 'dark')
+    }
+})
+
+
+// const changeTheme = function() {
+//     if (mode === 'dark') {
+//         mode = 'light';
+//         container.setAttribute('class', 'light');
+//     } else {
+//         mode = 'dark'
+//         container.setAttribute('class', 'dark')
+//     }
+// }
