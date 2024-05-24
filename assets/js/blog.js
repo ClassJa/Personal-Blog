d18161e635478fc3579d023f352dba6c069709bc
JA <69608172+ClassJay@users.noreply.github.com>
2024-05-24 00:16:08 -0500

    Debugging theme toggle

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,3 +1,6 @@
+const themeSwitcher2 = document.querySelector('#light-dark-mode-toggle');
+const container2 = document.querySelector('.container');
+
 const prevPage = document.querySelector("#back-button")
 
 // Figure out how to get the back button to connect to the click and take the user back to previous page
@@ -12,3 +15,18 @@ prevPage.addEventListener('click', function(){
 function renderBlogsToScreen() {
 
 }
+
+
+let mode = 'light';
+
+
+themeSwitcher2.addEventListener('click', function(){
+    console.log(mode)
+    if (mode === 'light') {
+        mode = 'dark';
+        document.body.setAttribute('class', 'dark');
+    } else {
+        mode = 'light'
+        document.body.setAttribute('class', 'light')
+    }
+})
